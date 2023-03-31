7a2c4f91c5f457096ba9e97dd4d5af96b2fee202
Ellis Marshall <ellis.marshall215@gmail.com>
2023-03-30 22:02:40 -0400

    Finished Functional Calculator

---
+++ app.js
@@ -0,0 +1,25 @@
+let result = document.getElementById('outputScreen');
+
+let calculate = (number) => {
+    result.value += number;
+} 
+
+result.value
+
+let Result = () => {
+    try{
+        result.value = eval(result.value)
+    }
+    catch(err){
+        alert('Enter a valid Input')
+    }
+}
+
+function clr(){
+    result.value = " ";
+}
+
+function del(){
+    result.value = result.value.slice(0,-1);
+}
+
